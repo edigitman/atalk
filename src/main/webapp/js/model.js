c3fd64afae8caaab9ee0665291f6d853b966e971
Alexandru Eduard Gitman <alexandru.eduard.gitman@ing.be>
2016-03-22 15:15:54 +0100

    first commit

---
+++ src/main/webapp/js/model.js
@@ -0,0 +1,81 @@
+// jquery plugin for enterKey
+$.fn.enterKey = function (fnc) {
+    return this.each(function () {
+        $(this).keyup(function (ev) {
+            var keycode = (ev.keyCode ? ev.keyCode : ev.which);
+            if (keycode == '13') {
+                fnc.call(this, ev);
+            }
+        })
+    })
+};
+
+// page View Model
+function TalkViewModel() {
+    var self = this;
+    inputText = ko.observable('');
+    titleInput = ko.observable('Name:');
+    isConnected = ko.observable(false);
+
+    var name = '';
+
+    var webSocket;
+    var output = $("#output");
+
+    this.openConnection = function () {
+
+        // open the connection if one does not exist
+        if (webSocket !== undefined && webSocket.readyState !== WebSocket.CLOSED) {
+            return;
+        }
+
+        if (inputText() == '') {
+            alert('Name mandatory !');
+            return;
+        }
+        name = inputText();
+        // Create a websocket
+        webSocket = new WebSocket("ws://45.32.184.20/talk/" + name);
+
+        webSocket.onopen = function (event) {
+            $("#userStatus").text("Connected as [" + name + "]!");
+            isConnected(true);
+            titleInput('Text:');
+            inputText('');
+            $("#input").focus();
+        };
+
+        webSocket.onmessage = function (event) {
+            self.updateOutput(event.data);
+        };
+
+        webSocket.onclose = function (event) {
+            self.updateOutput("Connection Closed");
+            isConnected(false);
+        };
+    };
+
+    this.updateOutput = function (text) {
+        output.append("<br/>" + text);
+        output.prop({scrollTop: output.prop("scrollHeight")});
+    };
+
+    this.send = function () {
+        if (webSocket != undefined && webSocket.readyState == WebSocket.OPEN && inputText() !== '') {
+            webSocket.send(inputText());
+            inputText('');
+            $("#input").focus();
+        }
+    };
+
+    $("#input").enterKey(function () {
+        inputText.valueHasMutated();
+        self.send();
+    });
+
+    window.setInterval(function() {
+        console.log('timer...');
+    }, 1000);
+}
+
+ko.applyBindings(new TalkViewModel());
